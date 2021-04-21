d328e6103c341d3004f69dd32a586fdd7717aa01
Jean Carlo <jeanccg14@outlook.com>
2021-04-21 12:22:59 -0500

    Euclides extendido 1.0

--- Teoria de Numeros/Algortimo-Extendido-Euclides/main.cpp
+++ Teoria de Numeros/Algortimo-Extendido-Euclides/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int algoritmoEuclides_ext(int a ,int b, int x1, int x2, int y1, int y2){
+int algoritmoEuclides_ext(int a ,int b, int *x1, int x2, int *y1, int y2){
+    int temp;
     if(b==0){
-
+        return a;
     }else{
-        algoritmoEuclides_ext(b,a%b,x,y);
+        cout<<a<<" "<<b<<" "<<*x1<<" "<<x2<<" "<<*y1<<" "<<y2<<endl;
+        temp=*x1;
+        *x1=x2;
+        x2=temp-(a/b)*x2;
+        temp=*y1;
+        *y1=y2;
+        y2=temp-(a/b)*y2;
+        algoritmoEuclides_ext(b,a%b,x1,x2,y1,y2);
     }
 }
 int main() {
-    int d,x=0,y=1;
-    algoritmoEuclides_ext(161,28,1,0,0,1);
-    cout<<d<<" "<<x<<" "<<y<<endl;
+    int x1=1,y1=0;
+    cout<<x1<<" "<<y1<<endl;
+    algoritmoEuclides_ext(161,28,&x1,0,&y1,1);
+    cout<<x1<<" "<<y1;
     return 0;
 }
